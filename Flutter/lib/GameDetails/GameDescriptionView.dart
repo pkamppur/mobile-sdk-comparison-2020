09e3492a055de01103a6b32bb9540a99653b9333
Tomi Koskinen <tomi.koskinen@reaktor.com>
2020-10-02 11:27:46 +0300

    code formatting changes

--- Flutter/lib/GameDetails/GameDescriptionView.dart
+++ Flutter/lib/GameDetails/GameDescriptionView.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:TheHotness/BGGApi/GameDetails.dart';
 
 class GameDescriptionView extends StatelessWidget {
-  GameDescriptionView({Key key, this.details}) : super(key: key);
-
   final GameDetails details;
+
+  GameDescriptionView({Key key, this.details}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(10),
-        alignment: Alignment.topLeft,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      padding: EdgeInsets.all(10),
+      alignment: Alignment.topLeft,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           Text('Description', style: Theme.of(context).textTheme.headline6),
           SizedBox(height: 5),
           Divider(
@@ -21,44 +22,50 @@ class GameDescriptionView extends StatelessWidget {
             thickness: 1,
           ),
           SizedBox(height: 10),
-          Container(
-              color: Colors.grey.shade100,
-              width: double.infinity,
-              padding: EdgeInsets.all(8),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: _category('Category', details.categories, context) +
-                      [SizedBox(height: 10)] +
-                      _category('Mechanisms', details.mechanisms, context) +
-                      [SizedBox(height: 10)] +
-                      _category('Family', details.families, context))),
+          _buildCategoryInfo(context),
           SizedBox(height: 10),
           Text(details.description,
               style: Theme.of(context)
                   .textTheme
                   .bodyText2
                   .apply(fontSizeFactor: 1.2))
-        ]));
+        ],
+      ),
+    );
   }
 
-  static _category(String title, List<Link> items, BuildContext context) {
-    return [
-          Text('Category',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyText1
-                  .apply(fontSizeFactor: 1.2)),
-          SizedBox(height: 3)
-        ] +
-        items
-            .expand((element) => [
-                  Text('${element.name}',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodyText2
-                          .apply(fontSizeFactor: 1.2)),
-                  SizedBox(height: 3)
-                ])
-            .toList();
-  }
+  Widget _buildCategoryInfo(BuildContext context) => Container(
+        color: Colors.grey.shade100,
+        width: double.infinity,
+        padding: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ..._category('Category', details.categories, context),
+            SizedBox(height: 10),
+            ..._category('Mechanisms', details.mechanisms, context),
+            SizedBox(height: 10),
+            ..._category('Family', details.families, context),
+          ],
+        ),
+      );
+
+  List<Widget> _category(
+          String title, List<Link> items, BuildContext context) =>
+      [
+        Text('Category',
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                .apply(fontSizeFactor: 1.2)),
+        SizedBox(height: 3),
+        ...items.expand((element) => [
+              Text('${element.name}',
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyText2
+                      .apply(fontSizeFactor: 1.2)),
+              SizedBox(height: 3)
+            ])
+      ].toList();
 }
