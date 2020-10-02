22b261eee634cd89e1baad083e640c6024e43454
Tomi Koskinen <tomi.koskinen@reaktor.com>
2020-10-02 10:59:02 +0300

    refactor game view number of players and game time

--- Flutter/lib/GameDetails/GameInfoView.dart
+++ Flutter/lib/GameDetails/GameInfoView.dart
@@ -9,50 +9,10 @@ class GameInfoView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.grey.shade300,
-        child: Column(children: [
-          Row(
-            children: [
-              Expanded(
-                  child: Container(
-                      decoration: BoxDecoration(
-                        border: Border(
-                          right: BorderSide(
-                              width: 1.0, color: Colors.grey.shade500),
-                        ),
-                        color: Colors.transparent,
-                      ),
-                      child: Container(
-                          padding: EdgeInsets.all(10),
-                          child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                    '${details.playersMin}–${details.playersMax} Players',
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        Theme.of(context).textTheme.headline6),
-                                Text('Best ${details.playersBest}',
-                                    textAlign: TextAlign.center,
-                                    style:
-                                        Theme.of(context).textTheme.bodyText1)
-                              ])))),
-              Expanded(
-                  child: Container(
-                      padding: EdgeInsets.all(0),
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                                '${details.playingTimeMin}–${details.playingTimeMax} Min',
-                                textAlign: TextAlign.center,
-                                style: Theme.of(context).textTheme.headline6),
-                            Text('Playing Time',
-                                textAlign: TextAlign.center,
-                                style: Theme.of(context).textTheme.bodyText1)
-                          ])))
-            ],
-          ),
+      color: Colors.grey.shade300,
+      child: Column(
+        children: [
+          GameFormatWidget(details: details),
           Divider(
             color: Colors.grey.shade500,
             height: 1,
@@ -64,20 +24,77 @@ class GameInfoView extends StatelessWidget {
             padding: EdgeInsets.all(10),
             child: Column(
               children: [
-                GameInfoContentLine(title: 'Designer: ',content: '${details.designers.map((e) => e.name).join(',')}',),
+                GameInfoContentLine(
+                  title: 'Designer: ',
+                  content: '${details.designers.map((e) => e.name).join(',')}',
+                ),
                 SizedBox(
                   height: 4,
                 ),
-                GameInfoContentLine(title: 'Artist: ', content: '${details.artists.map((e) => e.name).join(', ')}',),
-                SizedBox(
-                  height: 4
+                GameInfoContentLine(
+                  title: 'Artist: ',
+                  content: '${details.artists.map((e) => e.name).join(', ')}',
                 ),
-                GameInfoContentLine(title: 'Publisher: ', content: '${details.publishers.first.name} + ${details.publishers.length - 1} More',),
+                SizedBox(height: 4),
+                GameInfoContentLine(
+                  title: 'Publisher: ',
+                  content:
+                      '${details.publishers.first.name} + ${details.publishers.length - 1} More',
+                ),
               ],
             ),
           ),
-        ]));
+        ],
+      ),
+    );
   }
+}
+
+class GameFormatWidget extends StatelessWidget {
+  final GameDetails details;
+
+  const GameFormatWidget({
+    Key key,
+    @required this.details,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => IntrinsicHeight(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            buildRowItem(
+              context,
+              title: '${details.playersMin}–${details.playersMax} Players',
+              subtitle: 'Best ${details.playersBest}',
+            ),
+            VerticalDivider(thickness: 1, color: Colors.grey.shade500),
+            buildRowItem(
+              context,
+              title: '${details.playingTimeMin}–${details.playingTimeMax} Min',
+              subtitle: 'Playing Time',
+            ),
+          ],
+        ),
+      );
+
+  Widget buildRowItem(BuildContext context, {String title, String subtitle}) =>
+      Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(title,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline6),
+              Text(subtitle,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyText1)
+            ],
+          ),
+        ),
+      );
 }
 
 class GameInfoContentLine extends StatelessWidget {
@@ -101,12 +118,12 @@ class GameInfoContentLine extends StatelessWidget {
                 .bodyText1
                 .apply(fontSizeFactor: 1.2)),
         Expanded(
-            child: Text(
-              content,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2
-                    .apply(fontSizeFactor: 1.2)))
+          child: Text(content,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText2
+                  .apply(fontSizeFactor: 1.2)),
+        ),
       ],
     );
   }
