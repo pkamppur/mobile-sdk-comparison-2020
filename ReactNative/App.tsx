823058ab1e42e1234b8a819f039a9bfa5cdc81e4
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-12-26 20:55:34 +0200

    remove commented code

--- ReactNative/App.tsx
+++ ReactNative/App.tsx
@@ -15,34 +15,6 @@ import { MyStack } from './app/home';
 const App = () => {
   return (
     <NavigationContainer>
-      {/*<SafeAreaView style={backgroundStyle}>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
-          <Header />
-          <View
-            style={{
-              backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            }}>
-            <Section title="Step One">
-              Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-              screen and then come back to see your edits.
-            </Section>
-            <Section title="See Your Changes">
-              <ReloadInstructions />
-            </Section>
-            <Section title="Debug">
-              <DebugInstructions />
-            </Section>
-            <Section title="Learn More">
-              Read the docs to discover what to do next:
-            </Section>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-          */}
       <MyStack />
     </NavigationContainer>
   );
