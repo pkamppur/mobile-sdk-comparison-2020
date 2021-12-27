b4a4e21ffc59ae9227868f74f2d543568a97b31a
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-12-27 18:45:29 +0200

    prettier

--- ReactNative/app/BGGApi/BGGApi.ts
+++ ReactNative/app/BGGApi/BGGApi.ts
@@ -5,14 +5,16 @@ const baseUrl = 'http://localhost:38651/api';
 
 export namespace BGGApi {
   export const fetchTheHotness = async (): Promise<Game[]> => {
-    const response = await fetch(`${baseUrl}/the-hotness`)
-    const json = await response.json()
-    return json.items
+    const response = await fetch(`${baseUrl}/the-hotness`);
+    const json = await response.json();
+    return json.items;
   };
 
-  export const fetchGameDetails = async (gameId: string): Promise<GameDetails> => {
-    const response = await fetch(`${baseUrl}/games/${gameId}/details`)
-    const json = await response.json()
-    return json
+  export const fetchGameDetails = async (
+    gameId: string,
+  ): Promise<GameDetails> => {
+    const response = await fetch(`${baseUrl}/games/${gameId}/details`);
+    const json = await response.json();
+    return json;
   };
 }
