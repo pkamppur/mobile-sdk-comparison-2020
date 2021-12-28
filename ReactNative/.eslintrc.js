ad16886ba18690fbd236cb6a27bb3ba7194bae3c
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-12-28 19:35:39 +0200

    trying to fix VS Code warnings in detox tests

--- ReactNative/.eslintrc.js
+++ ReactNative/.eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['detox-bcp'],
+  env: {
+    'detox/detox': true,
+    jest: true,
+    'jest/globals': true,
+  },
   globals: {
-    "fetchMock": false
+    'fetchMock': false
   }
 };
