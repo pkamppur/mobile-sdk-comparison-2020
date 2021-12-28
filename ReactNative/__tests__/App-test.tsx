a225506415fd1c966829d535dc0e3676c1eeb54f
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-12-28 10:32:30 +0200

    component tests

--- ReactNative/__tests__/App-test.tsx
+++ ReactNative/__tests__/App-test.tsx
@@ -1,16 +1,53 @@
-/**
- * @format
- */
-
-jest.useFakeTimers();
-
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import { render, fireEvent } from '@testing-library/react-native';
+import theHotness from './mock-the-hotness.json';
+import gameDetails from './mock-game-details.json';
+import { NavigationContainer } from '@react-navigation/native';
+import { MyStack } from '../app/home';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+const appRootStack = () => (
+  <>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  </>
+);
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('Testing app screens', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  test('game list contains expected games', async () => {
+    fetchMock.mockResponseOnce(request => {
+      expect(request.url).toEqual('http://localhost:38651/api/the-hotness');
+      return Promise.resolve(JSON.stringify(theHotness));
+    });
+
+    const { findByText, getAllByText, getByText } = render(appRootStack());
+
+    const woodenFish = await findByText('Intelligent Wooden Fish');
+    const freshKeyboard = getByText('Tasty Fresh Keyboard');
+    const all2020 = getAllByText('2020');
+
+    expect(woodenFish).toBeTruthy();
+    expect(freshKeyboard).toBeTruthy();
+    expect(all2020.length).toBe(2);
+  });
+
+  test('tapping on game takes to details page', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(theHotness));
+
+    const { findByText } = render(appRootStack());
+    const woodenFish = await findByText('Intelligent Wooden Fish');
+
+    fetchMock.mockResponseOnce(JSON.stringify(gameDetails));
+
+    fireEvent(woodenFish, 'press');
+
+    const designerName = await findByText('Vincent Hand, Stella Kilback');
+
+    expect(designerName).toBeTruthy();
+  });
 });
