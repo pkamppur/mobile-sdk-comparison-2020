1de8414b7123dc55de1a9830ea428b2acebe08d8
Petteri Kamppuri <petteri.kamppuri@me.com>
2021-12-28 19:00:48 +0200

    mock api endpoints in UI / e2e tests

--- ReactNative/e2e/testApp.e2e.ts
+++ ReactNative/e2e/testApp.e2e.ts
@@ -1,7 +1,36 @@
 /* eslint-env detox/detox */
 import { device } from 'detox';
+import express from 'express';
+import http from 'http';
+import theHotness from '../__tests__/mock-the-hotness.json';
+import gameDetails from '../__tests__/mock-game-details.json';
+
+let server: http.Server;
 
 describe('Example', () => {
+  beforeAll(async () => {
+    const app = express();
+
+    app.get('/api/the-hotness', (req, res) => {
+      res.json(theHotness);
+    });
+
+    app.get('/api/games/10001/details', (req, res) => {
+      res.json(gameDetails);
+    });
+
+    await new Promise<void>(function (resolve) {
+      server = app.listen(38651, '127.0.0.1', () => {
+        console.log(` Running server on '${JSON.stringify(server.address())}'...`);
+        resolve();
+      });
+    });
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
   beforeAll(async () => {
     await device.launchApp();
   });
@@ -10,12 +39,10 @@ describe('Example', () => {
     await device.reloadReactNative();
   });
 
-  it('should have The Hotness list', async () => {
+  it('should show game list, and then game details after tap', async () => {
     await expect(element(by.text('The Hotness'))).toBeVisible();
-  });
 
-  it('should show game details after tap', async () => {
-    await element(by.text('Ark Nova')).tap();
-    await expect(element(by.text('Mathias Wigge'))).toBeVisible();
+    await element(by.text('Intelligent Wooden Fish')).tap();
+    await expect(element(by.text('Vincent Hand, Stella Kilback'))).toBeVisible();
   });
 });
